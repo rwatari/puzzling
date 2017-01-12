77f29d86e980bfb2b26c45fc29a6ed21a0d143c6
Raku Watari <rwatari@gmail.com>
2017-01-12 13:34:51 -0800

    Add team creation through form

--- frontend/components/user_home/team_index.jsx
+++ frontend/components/user_home/team_index.jsx
@@ -5,8 +5,12 @@ import {isEmpty} from 'lodash';
 class TeamIndex extends React.Component {
   constructor() {
     super();
-    this.state = {extraItems: 0};
-    this.handleExtra = this.handleExtra.bind(this);
+    this.state = {formShown: false, name: '', hunt: ''};
+    this.showForm = this.showForm.bind(this);
+    this.hideForm = this.hideForm.bind(this);
+    this.resetForm = this.resetForm.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -15,8 +19,30 @@ class TeamIndex extends React.Component {
     }
   }
 
-  handleExtra() {
-    this.setState({extraItems: this.state.extraItems + 1});
+  showForm() {
+    this.setState({formShown: true});
+  }
+
+  hideForm() {
+    this.setState({formShown: false});
+  }
+
+  resetForm() {
+    this.setState({formShown: false, name: '', hunt: ''});
+  }
+
+  handleChange(property) {
+    return e => {
+      this.setState({[property]: e.target.value});
+    };
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.createTeam({
+      name: this.state.name,
+      hunt_name: this.state.hunt
+    }).then(this.resetForm);
   }
 
   render() {
@@ -25,23 +51,43 @@ class TeamIndex extends React.Component {
       <TeamIndexItem key={id} team={teams[id]}/>
     ));
 
-    const moreItems = [];
-    for (let i = 0; i < this.state.extraItems; i++) {
-      moreItems.push(
-        <li
-          key={`extra${i}`}
-          className="item-card"
-          >This is extra {i}</li>);
+    let formItem;
+    if (this.state.formShown) {
+      formItem = (
+        <li key="form"
+          className="item-card">
+          <form onSubmit={this.handleSubmit} className="card-form">
+            <label>
+              <h5>Team name:</h5>
+              <input type="text"
+                value={this.state.name}
+                onChange={this.handleChange("name")}/>
+            </label>
+            <label>
+              <h5>Hunt name:</h5>
+              <input type="text"
+                value={this.state.hunt}
+                onChange={this.handleChange("hunt")}/>
+            </label>
+            <input type="submit" value="Add team!"/>
+            <button onClick={this.hideForm}>Cancel</button>
+          </form>
+        </li>
+      );
+    } else {
+      formItem = (
+        <li key="form"
+          className="item-card add-card"
+          onClick={this.showForm}>
+          <h4>Add a Team</h4>
+        </li>
+      );
     }
 
     return (
       <ul className="card-container">
-        <li
-          key="form"
-          className="item-card"
-          onClick={this.handleExtra}>+</li>
+        {formItem}
         {teamItems}
-        {moreItems}
       </ul>
     );
   }

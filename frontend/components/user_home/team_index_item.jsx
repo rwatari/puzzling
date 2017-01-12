662cd6d013725dc6f767df979b7ec12acd938706
Raku Watari <rwatari@gmail.com>
2017-01-12 15:04:52 -0800

    Add link to team card items

--- frontend/components/user_home/team_index_item.jsx
+++ frontend/components/user_home/team_index_item.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const TeamIndexItem = props => (
   <li className="item-card">
-    This is team {props.team.name}
+    <Link to={`/teams/${props.team.id}`}>
+      This is team {props.team.name}
+    </Link>
   </li >
 );
 

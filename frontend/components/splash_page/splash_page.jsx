8ebc5fd8879bd8b4ee26f3863e6179ed9af88240
Raku Watari <rwatari@gmail.com>
2017-01-20 00:57:27 -0800

    Add splash image and background

--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -4,9 +4,12 @@ import SplashPageNavContainer from '../nav_bars/splash_page_nav_container';
 const SplashPage = () => (
   <div className="splash">
     <main>
-      <div><h2>Solve puzzles together</h2></div>
+      <div>
+        <h2>Solve puzzles together</h2>
+        <br />
+        <h4>Manage your puzzle hunt teams easily and effectively with Puzzling! Join today and focus on the fun. Happy hunting!</h4>
+      </div>
     </main>
-    <footer>Happy Hunting!</footer>
   </div>
 );
 
